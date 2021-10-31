3fb4714d0b576b62fe4bb88d21d1e6964b8fa535
atif_bashir_98 <atif.bashr.1998@gmail.com>
2021-11-01 00:32:53 +0500

    updated title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello World! I am learning CI/CD with github actions.
         </p>
         <a
           className="App-link"
